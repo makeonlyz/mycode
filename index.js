7b521230c2aed0af44868f22417faa2d42853d49
makeonlyz <steamkeyshop@gmail.com>
2023-06-07 00:35:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,7 @@
-// Import the posts_new.json file
-const posts = require('./posts_new.json');
+module.exports = async (req, res) => {
+  const { PostID } = req.query;
 
-// Function to find a post by ID
-function findPostById(postId) {
-  return posts.find(post => post.postid === postId);
-}
-
-// Vercel serverless function
-module.exports = (req, res) => {
-  // Extract the PostID from the request URL
-  const postId = req.url.substring(1);
-
-  // Find the post with the given PostID
-  const post = findPostById(postId);
-
-  // If the post is found, return the title and image
-  if (post) {
-    const { title, metaValue } = post;
-    res.status(200).send(`
-      <h1>${title}</h1>
-      <img src="${metaValue}" alt="${title}">
-    `);
-  } else {
-    // If the post is not found, return a 404 response
-    res.status(404).send('Post not found');
-  }
+  res.status(200).send(`
+    <h1>Post ID: ${PostID}</h1>
+  `);
 };
